b82172f1f69a9ce150eb556afdda0cc6a8cf0c3d
omaddam <omaddam@gmail.com>
2020-12-22 23:52:21 -0700

    Created a categories sample.

--- Assets/App/Scripts/MainSceneManager.cs
+++ Assets/App/Scripts/MainSceneManager.cs
@@ -79,7 +79,7 @@ public class MainSceneManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Generates a scroll bar sample sample and displays it on the board.
+    /// Generates a scroll bar sample and displays it on the board.
     /// </summary>
     public void GenerateScrollBarSample()
     {
@@ -115,6 +115,53 @@ public class MainSceneManager : MonoBehaviour
 
         // Display board
         Board?.Initialize(board);
+    }
+
+    /// <summary>
+    /// Generates a categories sample and displays it on the board.
+    /// </summary>
+    public void GenerateCategoriesSample()
+    {
+        // Create a new board
+        KanbanBoard.DataStructure.Board board = new KanbanBoard.DataStructure.Board();
+
+        // Populate with pipelines
+        KanbanBoard.DataStructure.Pipeline toDoPipeline = new KanbanBoard.DataStructure.Pipeline("TO DO");
+        KanbanBoard.DataStructure.Pipeline inProgressPipeline = new KanbanBoard.DataStructure.Pipeline("IN PROGRESS");
+        KanbanBoard.DataStructure.Pipeline reviewPipeline = new KanbanBoard.DataStructure.Pipeline("REVIEW");
+        KanbanBoard.DataStructure.Pipeline donePipeline = new KanbanBoard.DataStructure.Pipeline("DONE");
+        board.Pipelines.AddRange(new List<KanbanBoard.DataStructure.Pipeline>()
+        {
+            toDoPipeline, inProgressPipeline, reviewPipeline, donePipeline
+        });
+
+        // Populate with categories
+        KanbanBoard.DataStructure.Category category1 = new KanbanBoard.DataStructure.Category("Category 1");
+        KanbanBoard.DataStructure.Category category2 = new KanbanBoard.DataStructure.Category("Category 2");
+        board.Categories.AddRange(new List<KanbanBoard.DataStructure.Category>()
+        {
+            category1, category2
+        });
+
+        // Populate with items
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category1));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category1));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, null));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category2));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category2));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category1));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), reviewPipeline, null));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, null));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category1));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, null));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), donePipeline, null));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category2));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category2));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), toDoPipeline, category2));
+        board.Items.Add(new KanbanBoard.DataStructure.Item(Guid.NewGuid(), reviewPipeline, category1));
+
+        // Display board
+        Board?.Initialize(board, true);
     }
 
     #endregion
